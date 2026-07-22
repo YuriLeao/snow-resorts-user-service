8041d85a8cf5f8447927970c6c0ba8a770cd2c69
YuriLeao <ydomingosleao@gmail.com>
2026-07-21 23:43:54 -0300

    Remove unused FriendshipStatus.BLOCKED enum value.
    Only PENDING and ACCEPTED are used in friendship flows.

--- src/main/java/com/snowresorts/user/domain/model/FriendshipStatus.java
+++ src/main/java/com/snowresorts/user/domain/model/FriendshipStatus.java
@@ -3,6 +3,5 @@ package com.snowresorts.user.domain.model;
 /** Lifecycle state of a friendship edge. Persisted as the upper-case enum name. */
 public enum FriendshipStatus {
     PENDING,
-    ACCEPTED,
-    BLOCKED
+    ACCEPTED
 }
